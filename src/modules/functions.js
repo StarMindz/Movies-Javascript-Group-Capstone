9d99e933c2fa9e6e5d4eb051c3fc21142641cd96
Solly <solobarine@gmail.com>
2022-09-09 11:47:47 +0100

    Fix Linters

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,14 +1,12 @@
 // Import Elements
-import {getComments, postComments} from './comments.js';
-import movies, {commentPopup} from './elements.js';
+import { getComments, postComments } from './comments.js';
+import movies, { commentPopup } from './elements.js';
 
 // Event Listener Function for Comment Button
 movies.addEventListener('click', (e) => {
   if (e.target.classList.contains('commentBtn-1')) {
     e.target.classList.add('test');
-    const target = e.target;
-    console.log(target);
-    const item = e.target.parentElement;
+    const target = e.target; // eslint-disable-line
     commentPopup.innerHTML = `<div class="c-popup">
                                 <p class="x">x</p>
                                 <img src="" alt="" class="poster">
@@ -21,47 +19,40 @@ movies.addEventListener('click', (e) => {
                                 <button class="submit">Comment</button>
                                 </div>`
   }
-  const pic = document.querySelector('.pic')
-  const try1 = document.querySelector('.comm')
-  console.log(pic);
-})
+});
 
 /// Event Listener for Comment Section Button
 commentPopup.addEventListener('click', (e) => {
-   if (e.target.classList.contains('submit')) {
-    const item = e.target.parentElement;
-     // Get Index of comment button
-    const buttons = document.getElementsByClassName('commentBtn-1')
+  if (e.target.classList.contains('submit')) {
+    // Get Index of comment button
+    const buttons = document.getElementsByClassName('commentBtn-1');
     const list = [];
-    for (let i = 0; i < buttons.length; i++) {
-       list.push(buttons[i].className);
+    for (let i = 0; i < buttons.length; i += 1) {
+      list.push(buttons[i].className);
     }
     const index = list.indexOf('commentBtn-1 test');
     // Get data from input fields
-    const userName = document.querySelector('.un')
-    const userComment = document.querySelector('.uc')
+    const userName = document.querySelector('.un');
+    const userComment = document.querySelector('.uc');
     // Send Comments to API
-    postComments(userName.value, userComment.value, index)
+    postComments(userName.value, userComment.value, index);
     // Recieve Comments from API
     getComments(index);
-   }
-})
+  }
+});
 
 // Show comments on page load
 movies.addEventListener('click', (m) => {
   if (m.target.classList.contains('commentBtn-1')) {
     m.target.classList.add('maybe');
     const btns = movies.getElementsByClassName('commentBtn-1');
-    console.log(btns)
     const lis = [];
-    for (let i = 0; i < btns.length; i++) {
-        lis.push(btns[i].className);
+    for (let i = 0; i < btns.length; i += 1) {
+      lis.push(btns[i].className);
     }
     const ind = lis.indexOf('commentBtn-1 maybe');
-    console.log(lis);
-    console.log(ind);
-   // console.log(ind);
+    // console.log(ind);
     // Get Comments
     getComments(ind);
   }
-})
+});
