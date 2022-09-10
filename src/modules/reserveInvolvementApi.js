f62c9de7c4b5a680855c00b042dd52bd7876664c
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-09-10 13:12:38 +0100

    Add the involvement Api for reserving movies

--- src/modules/reserveInvolvementApi.js
+++ src/modules/reserveInvolvementApi.js
@@ -3,11 +3,11 @@ const GAME_ID = '3bifdQ3qgzMtAvx1V3Pc';
 
 const getReservation = async (id) => {
   try {
-    const result = await fetch(`${BASE_URL}${GAME_ID}/reservations?item_id=${id};`);
+    const result = await fetch(`${BASE_URL}${GAME_ID}/reservations?item_id=${id}`);
     const data = await result.json();
     const { status } = result;
 
-    if (status !== 200) throw new Error('The pitition did not return 200');
+    if (status !== 200) throw new Error('The petition did not return 200');
 
     return data;
   } catch (error) {
@@ -19,13 +19,13 @@ const addReservation = async (id, name, start, end) => {
   const body = JSON.stringify({
     item_id: id,
     username: name,
-    start_date: start,
-    end_date: end,
+    date_start: start,
+    date_end: end,
 });
 
   const headers = { 'Content-type': 'application/json; charset=UTF-8' };
 
-  const result = await fetch(`${BASE_URL}${GAME_ID}/likes`, {
+  const result = await fetch(`${BASE_URL}${GAME_ID}/reservations`, {
     method: 'POST',
     body,
     headers,
