e4d28309501e86f6ea0eca7f4877ad74a3e34c9d
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-09-11 03:49:43 +0100

    merge branch

--- src/mocks/commentPop.js
+++ src/mocks/commentPop.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-const countComment = async(data) => {
-  return data.length;
-}
-=======
+
 const countComment = async (data) => data.length; // eslint-disable-line
->>>>>>> da5b4d5260dd46f00202b638f8469e7b06eabc2c
 
 export default countComment;
